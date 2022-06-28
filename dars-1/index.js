00bbe93ba6a3a1759f5a013e9ccc53c1bb5d2739
islomjon6975 <islomjon0919@mail.ru>
2022-06-28 18:39:50 +0500

    dars-1

---
+++ dars-1/index.js
@@ -0,0 +1,82 @@
+// Data Type -> malumot turi (primitive, non-primitive)
+// 6 ta ma'lumot turi bor: number, string, boolean, undefined, null, object,
+
+// primitve -> number, string, boolean, undefined, null -> bu faqat o'zida bitta ma'lumot saqlay oladi
+// number -> son, raqam -> 
+// string -> harf, so'z, teks -> qo'shtirnoq ichida yoziladi '' yoki ""
+// boolean -> true, false
+// undefined -> biror bir o'zgaruchi bor lekin unga hech qanday ma'lumot biriktirilmagan
+// null -> bu hech qanday ma'lumot turiga o'tmagan. 
+
+// non-primitive -> object
+// object -> birdan ortiq bo'lgan ma'lumotlarni o'zida jamlay oladigan tur
+
+
+// var, let, const
+// var -> oldindan ishlatib kelingan, change(ozgartirish), global scope va function scope
+// let -> Es6 versiya, change, block scope
+// const -> Es6 versiya, change qilib bo'lmaydi, xavfsiz o'zgaruvchi, 
+
+// hoisting -> o'zidan oldin chaqirish
+// var -> undefined qaytaradi
+// let, const -> error (Cannot access 'text' before initialization) qaytaradi
+
+
+// Number -> son, raqam
+// Amallar
+// + -> qoshish
+// - -> ayirish
+// * -> kopaytirish
+// / -> bolish
+// ** -> darajaga oshirib beradi va ildiz ostida qilib ishlatsak ham bo'ladi
+// % -> qoldiq chiqarib beradi
+// ++ -> bir qo'shib beradi
+// -- -> bir ayirib beradi
+// num++ 
+// num = num + 1
+// num--
+// num = num - 1
+
+
+// Funksiya
+// Number.parseInt() -> butun qismini olib beradi
+// Number.isInteger() -> son butun yoki butun emasligini aniqlab beradi
+// Number.parseFloat() -> butun va qoldiq qismini chiqarib beradi
+//  NaN -> son emas
+// toFixed() -> sonni kasr qismidan nechta olishini kerakligini, yaxlitlab beradi
+
+// console.log(Number.parseInt(5/2));
+// console.log(Number.isInteger(5));
+// console.log(Number.parseFloat(5/2));
+// console.log(12.325435234234543333333333.toFixed(2));\
+
+
+// masala
+
+// var a = 30;
+// var p = 4 * a;
+// console.log(p)
+
+
+// 8
+var a = 10;
+var b = 20;
+var average = (a + b) / 2
+
+console.log(average)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
