4bc4ab976e7a2a66865bd03db0374cce12e5d598
islomjon6975 <islomjon0919@mail.ru>
2022-07-19 18:03:45 +0500

    s

---
+++ dars-7/index.js
@@ -0,0 +1,30 @@
+// Array
+// [1,2,3,4, 'string', {name: 'Tom'}, [32,32,33]]
+// sort(), join(), slice(), splice(), contact(), split(), reverse(),
+// push(), pop(), unshift(), shift()
+// map(), forEach(), filter(), find(), findIndex(), reduce()
+
+let arr = [1,2,3,5]
+
+console.log(arr.filter((value,index) => value !== 2))
+
+// sort() -> sartirovka qilish, 2ta a va b, 2 xonali sonlar control qilish
+// join() -> stringga o'girib beradi
+// slice() -> copy paste
+// splice() -> qirqib oladi, main array tasir qiladi
+// concat() -> qoshib beradi
+// split() -> stringni arrayga aylantirib beradi
+// reverse() -> teskarisiga yozib beradi
+// push() -> oxiridan qoshib beradi 
+// pop() -> oxiridan ochirib beradi
+// unshift() -> boshidan qoshib beradi
+// shift() -> boshidan ochirib beradi
+
+// map() -> 2 holat - 1 - for loop vazifasi; 2 - update, va yangi array qaytaradi
+// forEach() -> for loop vazifasida keladi
+// filter() -> filtirlab berish, shart qanoatlantirsa yangi array qoshib beradi
+// find() -> qiymatni topib berish
+// findIndex() -> qiymatni indexini topib berish
+// reduce() -> array ichidagi sonlarni qo'shib beradi
+
+
